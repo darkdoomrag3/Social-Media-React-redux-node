5e10e809ed48a700f65d2d3d8a46296cb033c202
darkdoomrag3 <47889899+darkdoomrag3@users.noreply.github.com>
2020-08-18 19:19:50 +0430

    setup routers and adding login and register component

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,32 @@ import React from 'react';
 import './App.css'
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 const App = () => {
   return (
-    <React.Fragment>
-      <Navbar />
-      <Landing />
+    <Router>
 
-    </React.Fragment>
+      <React.Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+
+        </section>
+
+      </React.Fragment>
+    </Router>
+
   );
 }
 
 export default App;
+
+////// 6 5
+
